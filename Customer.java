124433e81a30584cb8732d1df46a3d17d897f978
siva-software-tester <sivasharp2610@gmail.com>
2024-10-25 21:25:58 +0530

    Initial commit

---
+++ Customer.java
@@ -0,0 +1,12 @@
+package com.constructor;
+
+public class Customer {
+	public String customerId;
+	public String customerName;
+	
+	public Customer(String custId, String custName) {
+		customerId=custId;
+		customerName=custName;
+	}
+
+}
---
+++ Test.java
@@ -0,0 +1,12 @@
+package com.constructor;
+
+public class Test {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Customer c= new Customer("1", "Siva");
+		System.out.println("CustomerID: "+c.customerId);
+		System.out.println("Customer Name: "+c.customerName);
+
+	}
+
+}
